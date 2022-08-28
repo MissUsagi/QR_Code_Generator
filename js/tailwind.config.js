fa357584447ef72b570e5191b938afb302821002
MissUsagi <syljanko@gmail.com>
2022-08-28 21:33:31 +0200

    init

---
+++ js/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+   theme: {
+      extend: {
+         colors: {
+            'teal': '#019097',
+            'peach': '#fea698',
+            'beige': '#fcf3e4',
+            'mint': '#7bc7ad',
+            'gray-dark': '#333',
+            'coffee': '#685338',
+         },
+         fontFamily: {
+            sans: ['Poppins', 'sans-serif']
+         },
+      }
+   }
+}
